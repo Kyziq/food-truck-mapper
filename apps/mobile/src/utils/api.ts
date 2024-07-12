f3e6d97dfaae248a5dc6b17c98ac5cec7bb2c74c
Haziq Khairi <ikhairulhaziq11@gmail.com>
2024-07-12 17:49:06 +0800

    🔇 refactor(api): remove log

--- apps/mobile/src/utils/api.ts
+++ apps/mobile/src/utils/api.ts
@@ -4,7 +4,6 @@ const apiBaseUrl = Constants.expoConfig?.extra?.apiBaseUrl;
 
 export const fetchFoodTrucks = async () => {
   try {
-    console.log(`Fetching from ${apiBaseUrl}/foodtrucks`); // Add this line
     const response = await fetch(`${apiBaseUrl}/foodtrucks`);
     const data = await response.json();
     return data;
