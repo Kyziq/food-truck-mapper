e0e1eb220b7865bd08488a32f6b75328ebc77b4e
Haziq Khairi <ikhairulhaziq11@gmail.com>
2024-07-14 18:02:12 +0800

    ✨ feat: add axios usage

--- apps/mobile/src/utils/api.ts
+++ apps/mobile/src/utils/api.ts
@@ -1,25 +1,36 @@
+import axios from "axios";
 import Constants from "expo-constants";
+import { FoodTruck, MenuItem } from "../types";
 
 const apiBaseUrl = Constants.expoConfig?.extra?.apiBaseUrl;
 
-export const fetchFoodTrucks = async () => {
+// Create an Axios instance with default configurations
+const axiosInstance = axios.create({
+  baseURL: apiBaseUrl,
+  headers: { "Content-Type": "application/json" },
+  timeout: 10000, // 10 seconds timeout
+});
+
+// Fetch a list of food trucks
+export const fetchFoodTrucks = async (): Promise<FoodTruck[]> => {
   try {
-    const response = await fetch(`${apiBaseUrl}/foodtrucks`);
-    const data = await response.json();
-    return data;
+    const response = await axiosInstance.get("/foodtrucks");
+    return response.data;
   } catch (error) {
     console.error("Failed to fetch food trucks:", error);
     return [];
   }
 };
 
-export const fetchMenuItems = async (foodTruckId: number) => {
+// Fetch menu items for a specific food truck by its ID
+export const fetchMenuItems = async (
+  foodTruckId: number
+): Promise<MenuItem[]> => {
   try {
-    const response = await fetch(
-      `${apiBaseUrl}/foodtrucks/${foodTruckId}/menuitems`
+    const response = await axiosInstance.get(
+      `/foodtrucks/${foodTruckId}/menuitems`
     );
-    const data = await response.json();
-    return data;
+    return response.data;
   } catch (error) {
     console.error("Failed to fetch menu items:", error);
     return [];
